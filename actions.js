909052c540a2399af797b2447afd384e175361e3
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2025-04-10 10:28:09 -0700

    Update actions.js

--- actions.js
+++ actions.js
@@ -4,13 +4,13 @@ let globals = {
   onChange: null
 }
 
-window.Poe.setupState = ({ initialState, actions, onChange }) => {
+const setupState = ({ initialState, actions, onChange }) => {
   globals.state = intialState;
   globals.actions = actions;
   globals.onChange = onChange;
 }
 
-window.Poe.action = (actionName, payload) => {
+const action = (actionName, payload) => {
   if (!globals.actions[actionName]) {
     throw new Error("Action not defined: "+actionName)
   }
@@ -18,21 +18,3 @@ window.Poe.action = (actionName, payload) => {
   globals.onChange(globals.state)
 }
 
-```
-Example for LLMs:
-
-window.Poe.setupState({
-   initialState: { todos: [] },
-   actions: {
-     addTodo: (todo, curState) => {
-        curState.todos.push(todo)
-     }
-   },
-   onChange(state => {
-      // implemented by us
-      updateUI(state)
-   })
-})
-
-window.Poe.action("addTodo", { text: "my todo", id: Math.random() })
-```
