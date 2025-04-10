f4a156cf6499d94b1bb5ab1ea313e805e2a7540c
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2025-04-10 10:28:49 -0700

    Update actions.js

--- actions.js
+++ actions.js
@@ -4,17 +4,18 @@ let globals = {
   onChange: null
 }
 
-const setupState = ({ initialState, actions, onChange }) => {
+export const setupState = ({ initialState, actions, onChange }) => {
   globals.state = intialState;
   globals.actions = actions;
   globals.onChange = onChange;
 }
 
-const action = (actionName, payload) => {
+export const action = (actionName, payload) => {
   if (!globals.actions[actionName]) {
     throw new Error("Action not defined: "+actionName)
   }
   globals.actions[actionName](payload, globals.state)
   globals.onChange(globals.state)
 }
+
 
