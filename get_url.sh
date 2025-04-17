b6a3298e0e4e0321f06ae0213bba36c620e793e8
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2025-04-17 02:58:22 -0700

    update the url

--- get_url.sh
+++ get_url.sh
@@ -4,4 +4,4 @@
 COMMIT_HASH=$(git rev-parse HEAD)
 
 # Output the URL with the commit hash
-echo "https://cdn.jsdelivr.net/gh/jeffreyyoung/poe_push_pull_state@${COMMIT_HASH}/client.js" 
+echo "https://cdn.jsdelivr.net/gh/jeffreyyoung/poe-db-prototype@${COMMIT_HASH}/app.js"
