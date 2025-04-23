e6a2a6706d280e49d563b60e063c8b97fb93c8e9
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2025-04-23 12:36:48 -0600

    update

--- _commit.sh
+++ _commit.sh
@@ -2,8 +2,6 @@ npx esbuild replicache.ts --bundle --format=esm --outfile=replicache.js
 npx esbuild replicache.ts --bundle --format=esm --outfile=public/replicache.js
 npx esbuild SyncedMap.ts --bundle --format=esm --outfile=SyncedMap.js
 npx esbuild SyncedMap.ts --bundle --format=esm --outfile=public/SyncedMap.js
-npx esbuild redux.ts --bundle --format=esm --outfile=redux.js
-npx esbuild redux.ts --bundle --format=esm --outfile=public/redux.js
 git add -A
 git commit -m "update"
 sh ./_update_readme.sh
