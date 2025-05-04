664452debed814c25b0788d82df9e0f2d3a18e27
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2025-05-04 14:31:45 -0600

    update

--- backend/replicache_server.ts
+++ backend/replicache_server.ts
@@ -2,7 +2,11 @@ import Ably from "npm:ably@2.8.0";
 import { DatabaseSync } from "node:sqlite";
 import { createServer } from "./replicache_server_core.ts";
 
-const db = new DatabaseSync(Deno.env.get("SQLITE_DB_PATH") || "test.db");
+const sqliteDbPath = Deno.env.get("SQLITE_DB_PATH") || "test.db";
+
+console.log("STARTING SERVER WITH SQLITE DB PATH", sqliteDbPath);
+
+const db = new DatabaseSync(sqliteDbPath);
 
 const ably = new Ably.Realtime("frBw7w.OhTF1A:ZQNStvW9BVmKiVwQ3ZqOtTN8T5-QaIlmkQ5a675c2iM");
 
