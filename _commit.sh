ae190d33bfdea214a47547ad9385b5f7c6c6730f
Jeffrey Young <7776643+jeffreyyoung@users.noreply.github.com>
2025-05-04 00:39:06 -0600

    update

--- _commit.sh
+++ _commit.sh
@@ -2,6 +2,10 @@ set -e
 
 deno task test
 
+deno task docker:up
+deno task test:docker
+deno task docker:down
+
 npx esbuild replicache.ts --bundle --format=esm --outfile=dist/replicache.js
 npx esbuild replicache.ts --bundle --format=esm --outfile=replicache.js
 git add -A
